54d337b0f4d71dc0402cccf4dd115a5d318a67d5
Ashish <you@example.com>
2026-03-26 11:50:04 +0530

    For and While loops

---
+++ StringPractice3.py
@@ -0,0 +1,29 @@
+#for lopps
+for i in(1,2,3,4,5):
+    print(f"Round of {i}")
+
+print("---------------------")
+items =(11,12,13,14,15)
+for items in items:
+    print(f"Round of {items}")
+
+print("---------------------")
+
+lang="Python"
+for lang in lang:
+    print(f"Round of {lang}")
+
+print("---------------------")
+
+# even
+for items in range (95,105,2):
+    print(f"Round of {items}")
+
+print("---------------------")
+
+#odd
+for num in range (1,10,2):
+    print(f"Round of {num}")
+
+
+
