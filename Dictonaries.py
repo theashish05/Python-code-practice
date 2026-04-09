2d0c4596d0462a22ed4e42c6dc337b56450a1313
Ashish <ashishacharyag305@gmail.com>
2026-04-09 13:47:00 +0530

    Dict methods

--- Dictonaries.py
+++ Dictonaries.py
@@ -11,3 +11,27 @@ my_dict['c']=40
 print(my_dict)
 
 # You are mutable 
+
+user = {'id':1,'age':30,'city': 'Bengaluru'}
+
+print(user['city'])
+print(user.get('age'))
+print('age'in user)
+print( 'id'not in user)
+
+print(user.keys())
+print(user.values())
+print(user.items())
+print(user)
+print('-----------------------------------------')
+
+
+for u in user :
+    print(u)
+
+for key,value in user.items():
+    print(key,value)
+
+
+user.update({'age':55,'city':'DVG'})
+print(user)
