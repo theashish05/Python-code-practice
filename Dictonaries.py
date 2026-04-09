f17732861ee527ec54bcacfca75f56bb37ec8669
Ashish <ashishacharyag305@gmail.com>
2026-04-09 12:40:59 +0530

    Dictonaries concept

---
+++ Dictonaries.py
@@ -0,0 +1,13 @@
+my_dict={'a':10,'b':20,'c':30}
+
+print(my_dict)
+# Ordered
+# Keys are unique
+# Values allow duplicates
+
+# Not indexed used by key value pair
+print(my_dict['b'])
+my_dict['c']=40
+print(my_dict)
+
+# You are mutable 
